b602fe344f9ad5b21c4a75373f5845de0beecef1
Степан Безносиков <steb1193@gmail.com>
2019-09-15 17:29:40 +0300

    init build

---
+++ webpack.config.js
@@ -0,0 +1,124 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const autoprefixer = require('autoprefixer')
+const isDevelopment = process.env.NODE_ENV !== 'production'
+module.exports = {
+    mode:  process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    entry: {
+        bundle: './src/app.js'
+    } ,
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: isDevelopment && "source-map",
+    devServer: {
+        port: 3000,
+        open: true,
+        contentBase: path.join(__dirname, "../src"),
+    },
+    module: {
+        rules: [
+            { test: /\.hbs$/, loader: "handlebars-loader" },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            autoprefixer: {
+                                browsers: ["last 2 versions"]
+                            },
+                            sourceMap: isDevelopment,
+                            minimize: !isDevelopment,
+                            plugins: () => [
+                                autoprefixer
+                            ]
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'dist/',
+                            useRelativePath: true,
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            optipng: {
+                                enabled: true,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        /** Since Webpack 4 */
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                handlebarsLoader: {}
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+
+        new HtmlWebpackPlugin({
+            title: 'My awesome service',
+            template: './src/index.hbs',
+            minify: !isDevelopment && {
+                html5: true,
+                collapseWhitespace: true,
+                caseSensitive: true,
+                removeComments: true,
+                removeEmptyElements: true
+            },
+        }),
+        new BrowserSyncPlugin({
+            // browse to http://localhost:3000/ during development,
+            // ./public directory is being served
+            host: 'localhost',
+            port: 3000,
+            server: { baseDir: ['./dist'] }
+        })
+    ]
+};
